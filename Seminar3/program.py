5cf02dd11d50eb3465626fcc0fb3b7427291fe27
MichaelSv <michaelsvistunoff@gmail.com>
2023-01-02 07:41:08 +0600

    Task 20 is comleted

--- Seminar3/program.py
+++ Seminar3/program.py
@@ -13,19 +13,19 @@ import random
 # 1 2 1 2 2
 # Вывод: 2
 
-n = int(input('Enter number of row: '))
+# n = int(input('Enter number of row: '))
 
-row = []
-for i in range(1, n):
-    row.append(random.randint(1, n/2))
-print(f'Your array: {row}')
+# row = []
+# for i in range(1, n):
+#     row.append(random.randint(1, n/2))
+# print(f'Your array: {row}')
 
-x = int(input('Enter number you need to check: '))
-count = 0
-for i in range(0, len(row)):
-    if row[i] == x:
-        count += 1
-print(f'Your number {x} meets {count} times in your row')
+# x = int(input('Enter number you need to check: '))
+# count = 0
+# for i in range(0, len(row)):
+#     if row[i] == x:
+#         count += 1
+# print(f'Your number {x} meets {count} times in your row')
 
 # Задача 18:
 # Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X.
@@ -39,20 +39,20 @@ print(f'Your number {x} meets {count} times in your row')
 # 1 2 1 8 9 6 5 4 3 4  
 # Вывод: 6
 
-n = int(input('Enter number of row: '))
-row = []
+# n = int(input('Enter number of row: '))
+# row = []
 
-for i in range(1, n):
-    row.append(random.randint(1, n))
-print(f'Your array: {row}')
+# for i in range(1, n):
+#     row.append(random.randint(1, n))
+# print(f'Your array: {row}')
 
-number = 0
-x = int(input('Enter number you need to check: '))
+# number = 0
+# x = int(input('Enter number you need to check: '))
 
-for i in range(0, len(row)):
-    if (x - row[i]) < x - number and x - row[i] > 0:
-        number = i
-print(row[number])
+# for i in range(0, len(row)):
+#     if (x - row[i]) < x - number and x - row[i] > 0:
+#         number = i
+# print(row[number])
 
 # Задача 20:
 # В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность.
@@ -79,3 +79,76 @@ print(row[number])
 # либо только русские буквы.  
 # Ввод: ноутбук  
 # Вывод: 12
+
+price = \
+    {
+        "A" : 1,
+        "E" : 1, 
+        "I" : 1, 
+        "O" : 1, 
+        "U" : 1, 
+        "L" : 1, 
+        "N" : 1, 
+        "S" : 1, 
+        "T" : 1, 
+        "R" : 1,        
+        "D" : 2,
+        "G" : 2,
+        "B" : 3,
+        "C" : 3,
+        "M" : 3,
+        "P" : 3,
+        "F" : 4,
+        "H" : 4,
+        "V" : 4,
+        "W" : 4,
+        "Y" : 4,  
+        "K" : 5,  
+        "J" : 8, 
+        "X" : 8,  
+        "Q" : 10, 
+        "Z" : 10,                
+        "А" : 1, 
+        "В" : 1, 
+        "Е" : 1, 
+        "И" : 1, 
+        "Н" : 1, 
+        "О" : 1, 
+        "Р" : 1, 
+        "С" : 1, 
+        "Т" : 1,        
+        "Д" : 2, 
+        "К" : 2, 
+        "Л" : 2, 
+        "М" : 2, 
+        "П" : 2, 
+        "У" : 2,          
+        "Б" : 3, 
+        "Г" : 3, 
+        "Ё" : 3, 
+        "Ь" : 3, 
+        "Я" : 3,          
+        "Й" : 4, 
+        "Ы" : 4,          
+        "Ж" : 5, 
+        "З" : 5, 
+        "Х" : 5, 
+        "Ц" : 5, 
+        "Ч" : 5,        
+        "Ш" : 8, 
+        "Э" : 8, 
+        "Ю" : 8,          
+        "Ф" : 10, 
+        "Щ" : 10, 
+        "Ъ" : 10
+    }
+
+word = input('Enter your word: ').upper()
+
+def Scrabble (word):
+    totalResult = 0
+    for letter in word:
+        totalResult += price[letter]
+    return totalResult
+
+print(f'Your word costs {Scrabble(word)} points')
